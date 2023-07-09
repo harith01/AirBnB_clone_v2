a4e8988eaef7314c04bc3c9afc07bf445c06a16e
harith01 <harithyusuph@gmail.com>
2023-07-09 11:49:50 +0000

    Task 1-

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,7 +3,6 @@
 
 from fabric.api import local
 from datetime import datetime
-from os.path import isdir
 
 def do_pack():
     """Generates archive file from the contents
@@ -11,8 +10,7 @@ def do_pack():
     time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     archive_name = 'web_static_{}.tgz'.format(time)
     try:
-        if isdir("versions") is False:
-            local("mkdir versions")
+        local("mkdir -p versions")
         archive_path = 'versions/{}'.format(archive_name)
         local("tar -cvzf {} web_static".format(archive_path))
         return archive_path
